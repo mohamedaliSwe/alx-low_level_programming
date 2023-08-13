0d1c4cc44505e04a77e4a89b09b051b0db05399c
Icelycoool <mohammedcali@proton.me>
2023-08-13 14:46:33 -0700

    Print hexidecimals

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = 0; hex < 10; hex++)
+		putchar(hex);
+	for (hex = 'a'; hex < 'h'; hex++)
+		putchar(hex);
+	putchar('\n');
+	return (0);
+}
