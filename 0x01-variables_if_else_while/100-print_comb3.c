1f9d4e5edd7516e70f9c3645427a3b2fda09b8c5
Icelycoool <mohammedcali@proton.me>
2023-08-13 15:56:49 -0700

    Print combo 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,27 +6,26 @@
  */
 int main(void)
 {
-    int num1;
-    
-    for (num1 = 0; num1 <= 8; num1++)
-    {
-        int num2;
-        
-        for (num2 = num1 + 1; num2 <= 9; num2++)
-        {
-            putchar(num1 + '0');
-            putchar(num2 + '0');
-            
-            if (num1 != 8 || num2 != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	int num1;
 
-    putchar('\n');
+	for (num1 = 0; num1 <= 8; num1++)
+	{
+		int num2;
 
-    return 0;
+		for (num2 = num1 + 1; num2 <= 9; num2++)
+		{
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+
+			if (num1 != 8 || num2 != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return 0;
 }
-
