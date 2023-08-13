44e7ebf018c85da79cc5a39c7a933f94122048ed
Icelycoool <mohammedcali@proton.me>
2023-08-13 15:09:24 -0700

    Print combo

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-		putchar(n + '0');
 	{
+		putchar(n + '0');
 		if (n < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+		putchar('\n');
+		return (0);
 	}
-	putchar('\n');
-	return (0);
 }
