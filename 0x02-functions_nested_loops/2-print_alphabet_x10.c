74430bb232adbc6d59807950461ecac1ae91fc06
Icelycoool <mohammedcali@proton.me>
2023-08-15 08:13:02 -0700

    Print the alphabets in small letter 10 times followed by new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,13 @@ void print_alphabet_x10(void)
 	int letter;
 	int counter = 0;
 
-	while (counter <= 10)
+	while (counter < 10)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
 
 		_putchar('\n');
-	
+
 		counter++;
 	}
 }
