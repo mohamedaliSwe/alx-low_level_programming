0072676c7fff444cb1e2225fc9f562c24e769d3f
Icelycoool <mohammedcali@proton.me>
2023-08-15 08:51:16 -0700

    Check for small letter character

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,10 @@
 #include "main.h"
 
 /**
-*int _islower(int c)-checks for lowercase character.
+* _islower - Checks if a character is lowercase
+* @c: The character to be checked
+*
+* Return: 1 if c is lowercase, 0 otherwise
 */
 int _islower(int c)
 {
