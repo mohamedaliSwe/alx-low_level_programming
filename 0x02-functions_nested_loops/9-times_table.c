d25df47921093d4a783229e0a3dfd49b1ca0ddd3
Icelycoool <mohammedcali@proton.me>
2023-08-15 15:01:46 -0700

    Multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,22 +13,26 @@ void times_table(void)
 		{
 			int product = i * j;
 
-			if (j > 0)
+			if (j == 0)
 			{
-				_putchar(',');
-				_putchar(' ');
-			}
-
-			if (product < 10)
-			{
-				_putchar(' ');
+				_putchar('0' + product);
 			}
 			else
 			{
-				_putchar((product / 10) + '0');
-			}
+				_putchar(',');
+				_putchar(' ');
 
-			_putchar((product % 10) + '0');
+				if (product < 10)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((product / 10) + '0');
+				}
+
+				_putchar((product % 10) + '0');
+			}
 
 			if (j == 9)
 			{
@@ -37,4 +41,3 @@ void times_table(void)
 		}
 	}
 }
-
