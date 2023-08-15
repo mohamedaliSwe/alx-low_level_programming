499dc0ae98b4e0e48573929693cf6996c0895e25
Icelycoool <mohammedcali@proton.me>
2023-08-15 08:00:53 -0700

    Print the alphabets in small letter 10 times followed by new line

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
-*print_alphabet_x10-prints 10 times the alphabet, in lowercase, followed by a new line.
+*print_alphabet_x10-prints 10 times the alphabet.
 */
 void print_alphabet_x10(void)
 {
 	int letter;
 	int counter = 0;
 
-	while (counter < 10)
+	while (counter <= 10)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 			_putchar(letter);
