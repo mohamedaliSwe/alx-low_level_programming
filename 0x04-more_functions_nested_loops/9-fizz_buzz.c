a08de8ff7ed79d5cdb5cba2cd6881406eb74322c
Icelycoool <mohammedcali@proton.me>
2023-08-17 16:21:38 -0700

    FizzBuzz Test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,30 +9,32 @@
 */
 int main(void)
 {
-    for (int i = 1; i <= 100; i++)
-    {
-        if (i % 3 == 0 && i % 5 == 0)
-	{
-		printf(" FizzBuzz");
-	}
-	else if (i % 3 == 0)
-	{
-		printf(" Fizz");
-	}
-	else if (i % 5 == 0)
-	{
-		printf(" Buzz");
-	}
-	else if (i == 1)
-	{
-		printf("%d", i);
-	}
-	else
-	{
-		printf(" %d", i);
-	}
-    }
-    printf("\n");
+	int i;
 
-    return (0);
+	for (int i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+
+	return (0);
 }
