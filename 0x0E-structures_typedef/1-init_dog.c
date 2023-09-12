08f645220efef6936c84c1f35830382c86b6c252
Icelycoool <mohammedcali@proton.me>
2023-09-12 15:10:36 -0700

    A function that initialize a variable of type struct dog

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-	#include "dog.h"
+#include "dog.h"
 
 /**
  * init_dog - initializes a variable of type struct dog
@@ -7,6 +7,8 @@
  * @name: name to initialize
  * @age: age to initialize
  * @owner: owner to initialize
+ *
+ * Return: None
  */
 void init_dog(struct dog **d, char *name, float age, char *owner)
 {
@@ -14,10 +16,33 @@ void init_dog(struct dog **d, char *name, float age, char *owner)
 	{
 		*d = malloc(sizeof(struct dog));
 		if (*d == NULL)
-			return;
+		{
+			/* Handle memory allocation failure */
+			perror("malloc");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		/* If *d is not NULL, free existing memory to avoid memory leaks */
+		free((*d)->name);
+		free((*d)->owner);
 	}
 
-	(*d)->name = name;
+	(*d)->name = strdup(name); /* Allocate and copy name */
+	if ((*d)->name == NULL)
+	{
+		/* Handle memory allocation failure */
+		perror("strdup");
+		exit(EXIT_FAILURE);
+	}
+
 	(*d)->age = age;
-	(*d)->owner = owner;
+	(*d)->owner = strdup(owner); /* Allocate and copy owner */
+	if ((*d)->owner == NULL)
+	{
+		/* Handle memory allocation failure */
+		perror("strdup");
+		exit(EXIT_FAILURE);
+	}
 }
